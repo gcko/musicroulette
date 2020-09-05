1d7cdd66d328f224c52a0fd4e376689c4101a38e
Jared Scott <jared.scott@variable.team>
2020-09-05 13:12:38 +0800

    Make a Template config file, and gitignore config.js as that will be the correct one moving forward

---
+++ app/js/configTemplate.js
@@ -0,0 +1,11 @@
+/* globals window, document */
+
+// TODO Please rename this file to `config.js` and include your own API Keys.
+//  check https://console.developers.google.com/apis/credentials for more information
+
+const Config = (() => {
+  return {
+    YOUTUBE_API_KEY: 'YOUR_YOUTUBE_API_KEY',
+    CLIENT_ID: 'YOUR_CLIENT_ID',
+  };
+})();
