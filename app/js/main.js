00f601741a277bb1cafd8dfaac6f5a21c38145a8
Jared Scott <jared.scott@variable.team>
2020-09-05 13:07:21 +0800

    Pull API Keys into their own configuration, put in dummy API keys as this is a public repository

--- app/js/main.js
+++ app/js/main.js
@@ -35,9 +35,8 @@ function getKindOfRandomNum(upperBound = 50) {
   const KEY_MUSIC = 'music',
     KEY_WEDDING = 'wedding',
     KEY_TOP10 = 'top10',
-    YOUTUBE_API_KEY = '***REMOVED***',
-    CLIENT_ID =
-      '900938017335-jpaqa4r02mjulqk40dcsa7ejak7fbk8v.apps.googleusercontent.com',
+    YOUTUBE_API_KEY = Config.YOUTUBE_API_KEY,
+    CLIENT_ID = Config.CLIENT_ID,
     loginContainer = document.getElementById('login-container'),
     btnLogin = document.getElementById('login'),
     btnWedding = document.getElementById('wedding-music'),
